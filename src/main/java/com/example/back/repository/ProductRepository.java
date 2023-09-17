8dc43e730502be77a2a8d0baa0098cbc95e940f8
samlogy <aceportgasd235@gmail.com>
2023-09-17 11:08:21 +0100

    add: flyway

--- src/main/java/com/example/back/repository/ProductRepository.java
+++ src/main/java/com/example/back/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.example.back.repository;
 
 import com.example.back.model.Product;
+<<<<<<< HEAD
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
